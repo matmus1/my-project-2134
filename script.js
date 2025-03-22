24db010fe213e45b9679e179ad0d31a5ad2dae41
matmus1 <matmus2011@gmail.com>
2025-03-22 22:04:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function startGame() {
+    alert("Game Starting..."); // Replace with actual game logic
+}
+function openSettings() {
+    alert("Opening Settings...");
+}
+function exitGame() {
+    alert("Exiting Game...");
+}
+
