bffe0a69b7ae706aeb562dd9d80d93dd458542fa
matmus1 <matmus2011@gmail.com>
2025-03-23 11:07:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function startGame() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('gameScreen').style.display = 'block';
     alert("Game Starting..."); // Replace with actual game logic
 }
+
+function goBack() {
+    document.getElementById('gameScreen').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+}
+
 function startODB() {
     alert("Opening ODB...");
 }
+
 function startProfile() {
     alert("Opening Profile...");
 }
+
 function startSettings() {
     alert("Opening Settings...");
 }
+
 function startSupporters() {
     alert("Opening Supporters...");
 }
