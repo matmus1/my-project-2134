a5f31c7e784fdd87ab7530620fa5fee29bd1e305
matmus1 <matmus2011@gmail.com>
2025-05-27 12:14:03 +0200

    Update script.js0.2

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function switchToLogin() {
 }
 
 function login() {
-    isLoggedIn = true;
     alert("You have logged in successfully!");
     toggleLoginModal();
     document.getElementById("loginBtn").innerText = "Logout";
@@ -34,28 +33,57 @@ function register() {
     switchToLogin();
 }
 
-
-// Show selected screen and hide others
 function showScreen(screenId) {
     document.getElementById('menu').style.display = 'none';
-
     const allScreens = document.querySelectorAll('.game-screen');
     allScreens.forEach(screen => screen.style.display = 'none');
-
     document.getElementById(screenId).style.display = 'block';
 }
 
-// Return to main menu
 function goBack() {
     const allScreens = document.querySelectorAll('.game-screen');
     allScreens.forEach(screen => screen.style.display = 'none');
-
     document.getElementById('menu').style.display = 'block';
 }
 
-// Toggle license text visibility
 function toggleLicense(event) {
     event.preventDefault();
     const license = document.getElementById("licenseText");
     license.classList.toggle("visible");
+}
+
+function startSolo() {
+    alert('Solo mode selected!');
+}
+
+function startMulti() {
+    alert('Multiplayer mode selected!');
+}
+
+function startCreateArmy() {
+    alert('Create Army selected!');
+}
+
+function startCreateProfile() {
+    alert('Create Profile selected!');
+}
+
+function startEdite() {
+    alert('Edit Profile selected!');
+}
+
+function startStats() {
+    alert('Stats selected!');
+}
+
+function startGraphics() {
+    alert('Graphics settings!');
+}
+
+function startAudio() {
+    alert('Audio settings!');
+}
+
+function startGameplay() {
+    alert('Gameplay settings!');
 }
