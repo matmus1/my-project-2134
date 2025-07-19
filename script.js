b64021676933256c9ee61e9c5f57f40f73d126bd
matmus1 <matmus2011@gmail.com>
2025-07-19 14:26:37 +0200

    Update script.js0.214

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-function showScreen(screenId) {
-    document.getElementById('menu').style.display = 'none';
-
-    const allScreens = document.querySelectorAll('.game-screen');
-    allScreens.forEach(screen => screen.style.display = 'none');
-
-    document.getElementById(screenId).style.display = 'block';
-}
-
-function goBack() {
-    const allScreens = document.querySelectorAll('.game-screen');
-    allScreens.forEach(screen => screen.style.display = 'none');
-
-    document.getElementById('menu').style.display = 'block';
-}
-
-function toggleLicense(event) {
-    event.preventDefault();
-    const license = document.getElementById("licenseText");
-    license.classList.toggle("visible");
-}
-
 function startSolo() {
     alert('Solo mode selected!');
 }
