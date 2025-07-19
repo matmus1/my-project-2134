69b6bdc912ff5d4b900ba609b6a59d5742d5d43e
matmus1 <matmus2011@gmail.com>
2025-07-19 19:52:26 +0200

    Update units.js0.215

--- game/units.js
+++ game/units.js
@@ -1,1 +1,19 @@
+const units = [
+  { x: 5, y: 5, type: 'infantry', selected: false }
+];
+
+function drawUnits() {
+  units.forEach(unit => {
+    ctx.fillStyle = unit.selected ? "yellow" : "white";
+    ctx.fillRect(unit.x * TILE_SIZE + 10, unit.y * TILE_SIZE + 10, 20, 20);
+  });
+}
+
+function gameLoop() {
+  drawGrid();
+  drawUnits();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
