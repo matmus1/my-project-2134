a5c8b06e46eeb0bf933e6b6730108da76a70506a
matmus1 <matmus2011@gmail.com>
2025-07-19 14:13:34 +0200

    Update script.js0.214

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-let isLoggedIn = false;
-
-function toggleLogin() {}
-
-function toggleLoginModal() {
-    const modal = document.getElementById("loginModal");
-    modal.style.display = modal.style.display === "flex" ? "none" : "flex";
-}
-
-function switchToRegister() {
-    document.getElementById("loginForm").style.display = "none";
-    document.getElementById("registerForm").style.display = "block";
-}
-
-function switchToLogin() {
-    document.getElementById("registerForm").style.display = "none";
-    document.getElementById("loginForm").style.display = "block";
-}
-
-function login() {
-    isLoggedIn = true;
-    alert("You have logged in successfully!");
-    toggleLoginModal();
-    document.getElementById("loginBtn").innerText = "Logout";
-}
-
-function register() {
-    alert("Konto utworzone!");
-    switchToLogin();
-}
-
 function showScreen(screenId) {
     document.getElementById('menu').style.display = 'none';
 
