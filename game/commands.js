634428e487189ae1ef3d2e826b88943be7b9b6a2
matmus1 <matmus2011@gmail.com>
2025-07-21 21:11:37 +0200

    Update commands.js

--- game/commands.js
+++ game/commands.js
@@ -1,19 +1,2 @@
-canvas.addEventListener("click", (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const x = Math.floor((e.clientX - rect.left) / TILE_SIZE);
-  const y = Math.floor((e.clientY - rect.top) / TILE_SIZE);
 
-  let selectedUnit = units.find(unit => unit.x === x && unit.y === y);
-  if (selectedUnit) {
-    units.forEach(unit => unit.selected = false);
-    selectedUnit.selected = true;
-  } else {
-    // ruch
-    const unit = units.find(u => u.selected);
-    if (unit) {
-      unit.x = x;
-      unit.y = y;
-    }
-  }
-});
 
