b565ef1d71dd34f162de4b3d3955bcd94f5c0566
matmus1 <matmus2011@gmail.com>
2025-07-23 20:10:17 +0200

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,12 +7,15 @@ function showScreen(screenId) {
     document.getElementById(screenId).style.display = 'block';
 }
 
+function goBack() {
+    const allScreens = document.querySelectorAll('.game-screen');
+    allScreens.forEach(screen => screen.style.display = 'none');
+
+    document.getElementById('menu').style.display = 'block';
+}
+
 function toggleLicense(event) {
     event.preventDefault();
     const license = document.getElementById("licenseText");
     license.classList.toggle("visible");
-}
-
-function goBack() {
-    showScreen('menu');
 }
